de299927f9293036646cc249a6874ab84bde19d5
Rafael09ED <starmarstrek@gmail.com>
2020-07-11 16:52:38 -0400

    Updates

---
+++ precache-manifest.e1a7974a505a9f63cca11e7648b61c42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8838d6a2d0b5ea3e979d72b5d3032846",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58eb5228df2b391ed2e9",
+    "url": "/static/css/main.342e84a7.chunk.css"
+  },
+  {
+    "revision": "a5144b16997d336efd37",
+    "url": "/static/js/2.80f67bad.chunk.js"
+  },
+  {
+    "revision": "bad14811e184aea199be0b4982714a93",
+    "url": "/static/js/2.80f67bad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58eb5228df2b391ed2e9",
+    "url": "/static/js/main.981b393d.chunk.js"
+  },
+  {
+    "revision": "54c817c7363ddcfd880e",
+    "url": "/static/js/runtime-main.9d984272.js"
+  }
+]);
